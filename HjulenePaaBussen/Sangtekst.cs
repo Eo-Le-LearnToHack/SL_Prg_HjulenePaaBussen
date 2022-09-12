5bbb14d3fa68201469678cd524209322f042a8ec
Dan Nguyen <Dan.Nguyen@Rsyd.Dk>
2022-09-12 11:52:23 +0200

    add initial lyric text to SangTekst

--- HjulenePaaBussen/Sangtekst.cs
+++ HjulenePaaBussen/Sangtekst.cs
@@ -1,8 +1,52 @@
 ﻿using System;
+using System.Text;
 
 public class Sangtekst
 {
 	public Sangtekst()
 	{
-	}
+        this.UdskrivSangTekst();
+
+    }
+
+    private List<string> versPrimaer = new List<string>
+	{
+        "Hjulene på bussen drejer ",
+        "Dørene på bussen siger ",
+        "Pengene i bussen siger ",
+        "Fruerne i bussen siger ",
+        "Børnene i bussen siger ",
+        "Hornet i bussen siger "
+
+    };
+	
+	private List<string> versGentagelser = new List<string>
+	{
+		"rundt, rundt, rundt",
+        "åben og luk, åben og luk, åben og luk",
+        "klir, klir, klir",
+        "snak, snak, snak",
+        "vræh, vræh, vræh",
+        "båt, båt, båt"
+
+    };
+
+    private string versSlut = "Gennem hele byen";
+
+    private void UdskrivSangTekst ()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append(this.versPrimaer[0] + this.versGentagelser[0]);
+        string versPrimaerTekst = sb.ToString();
+        Console.WriteLine(versPrimaerTekst);
+
+        string firstLetterUpperCase = versGentagelser[0].Substring(0).ToUpper();
+        string restLettersLowerCase = versGentagelser[0].Substring(1, versGentagelser[0].Length).ToUpper();
+
+        sb.Clear();
+        sb.Append(firstLetterUpperCase + restLettersLowerCase);
+        string versGentagelserTekst = sb.ToString();
+        Console.WriteLine(versGentagelserTekst);
+    }
+
 }
